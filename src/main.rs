01a65e0bf87d13a5a0a8eebdf004a41664ffe099
Andrii Tantsiura <antostcr@gmail.com>
2026-02-20 05:55:21 +0100

    now main is a loop with smart prints that wont spam

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::{self, HashSet},
+    thread, time,
+};
+
 use crate::{
     process::Suspicious,
     scanner::{get_process, scan_processes},
@@ -6,23 +11,34 @@ use crate::{
 mod process;
 mod scanner;
 // main loop here
-// just scans processes right now
-// also prints how many active processes are working right now
+// scans processes and prints suspicious
+// doesnt print anything twice if found
+// for tests:
+// sleep 1000 & -> returns pid
+// sudo strace -p returned_pid
+// cargo run must print sleep process
 fn main() {
-    match scan_processes() {
-        Ok(vec) => {
-            println!("{}", vec.len());
-            for pid in vec {
-                match get_process(pid) {
-                    Ok(proc) => {
-                        if proc.is_suspicious() {
-                            println!("{:?}", proc);
+    let mut found: HashSet<u64> = collections::HashSet::new();
+    loop {
+        match scan_processes() {
+            Ok(vec) => {
+                for pid in vec {
+                    match get_process(pid) {
+                        Ok(proc) => {
+                            if found.contains(&pid) && !proc.is_suspicious() {
+                                found.remove(&pid);
+                            }
+                            if proc.is_suspicious() && !found.contains(&pid) {
+                                println!("{:?}", proc);
+                                found.insert(pid);
+                            }
                         }
-                    }
-                    Err(_) => {}
-                };
+                        Err(_) => {}
+                    };
+                }
             }
+            Err(_) => {}
         }
-        Err(_) => {}
+        thread::sleep(time::Duration::from_secs(5));
     }
 }
